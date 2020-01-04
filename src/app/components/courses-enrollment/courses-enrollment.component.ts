a4c367e2e42bafd2b788f71dcc505d19cb2059f7
narcici <narcisa.teodora16@gmail.com>
2020-01-04 18:53:09 +0200

    enrollment

--- src/app/components/courses-enrollment/courses-enrollment.component.ts
+++ src/app/components/courses-enrollment/courses-enrollment.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {CourseCard} from '../../core/models/course-card';
 import {CoursesService} from '../../core/services/courses.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-courses-enrollment',
@@ -10,10 +11,15 @@ import {CoursesService} from '../../core/services/courses.service';
 export class CoursesEnrollmentComponent implements OnInit {
 
   courses: CourseCard[];
+  creditsToFullFill = 30;
+  chosenCredits = 0;
+  tooMany = false;
+
+  popoverIsVisible = false;
 
   @Output() viewCourse: EventEmitter<CourseCard> = new EventEmitter<CourseCard>();
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService,  private router: Router) { }
 
   ngOnInit() {
     this.courses = this.coursesService.getMoreCourses();
@@ -22,5 +28,31 @@ export class CoursesEnrollmentComponent implements OnInit {
   sendCourse(course: CourseCard) {
     this.viewCourse.emit(course);
     console.log(course);
+  }
+
+  enroll() {
+    if (this.chosenCredits === this.creditsToFullFill) {
+       this.router.navigateByUrl('courses');
+    }
+  }
+
+  selectCourse(course) {
+    if (!course.isLocked && !course.isCompleted) {
+      if (this.chosenCredits + course.numberOfCredits > this.creditsToFullFill) {
+        this.tooMany = true;
+      } else {
+        this.chosenCredits = this.chosenCredits + course.numberOfCredits;
+        this.tooMany = false;
+
+      }
+    }
+  }
+
+  showPopover() {
+      this.popoverIsVisible = true;
+  }
+
+  hidePopover() {
+    this.popoverIsVisible = false;
   }
 }
