d8228de62b5b6e72744a5a99d70055e16d18ac2f
narcici <narcisa.teodora16@gmail.com>
2020-01-08 13:00:52 +0200

    from checkbox to radio

--- src/app/components/professor-page/professor-page.component.ts
+++ src/app/components/professor-page/professor-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {style} from '@angular/animations';
 
 @Component({
   selector: 'app-professor-page',
@@ -8,17 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class ProfessorPageComponent implements OnInit {
 
   icons = ['angry', 'tired', 'grimace', 'smile-beam', 'grin-hearts'];
-  selectedIcon = '';
+  selectedIcon = 'smile-beam';
   displayedIcon = 'smile-beam';
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  selectIcon( newSelectedIcon) {
+  selectIcon(newSelectedIcon) {
     console.log('choosen ' + newSelectedIcon);
     this.selectedIcon = newSelectedIcon;
     console.log('set to ' + this.selectedIcon);
+
+    document.getElementById(newSelectedIcon).style.fontSize = '20px';
+
+    let i = 0;
+    for (i = 0; i < 5; i++) {
+      if (this.selectedIcon !== this.icons[i]) {
+        document.getElementById(this.icons[i]).style.fontSize = '15px';
+      }
+    }
+
   }
 
   getDisplayedIcon(index) {
@@ -28,5 +41,8 @@ export class ProfessorPageComponent implements OnInit {
   submit() {
     this.displayedIcon = this.selectedIcon;  // aici se updateaza iconul la cel selectat recent
     console.log('submit is pressed');
+
+    // reset the size of the selected icon also
+    document.getElementById(this.selectedIcon).style.fontSize = '15px';
   }
 }
